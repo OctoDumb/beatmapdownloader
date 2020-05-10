7fbc24a179d036064bda30fffaeae3936ee81d82
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2020-05-11 01:38:18 +0300

    Fix maximize button and add scroll to maps list

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import 'normalize.css';
 import './App.scss';
 import Content from './components/Content/Content';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faDownload, faTimes, faWindowMinimize, faWindowMaximize } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faTimes, faWindowMinimize, faWindowMaximize, faWindowRestore } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faDownload, faTimes, faWindowMaximize, faWindowMinimize);
+library.add(faDownload, faTimes, faWindowMaximize, faWindowMinimize, faWindowRestore);
 
 export default class App extends Component {
   render() {
--- src/components/WindowControl/WindowControlButtons/WindowControlButtons.jsx
+++ src/components/WindowControl/WindowControlButtons/WindowControlButtons.jsx
@@ -6,15 +6,30 @@ export default class WindowControlButtons extends Component {
         super();
 
         this.win = window.electron.remote.getCurrentWindow();
+
+        this.state = {
+            isMaximized: this.win.isMaximized()
+        }
     }
+
+    windowMaximize() {
+        if (!this.state.isMaximized) {
+            this.win.maximize()
+            this.setState({isMaximized: true})
+        } else {
+            this.win.unmaximize()
+            this.setState({isMaximized: false})
+        } 
+    }
+
     render() {
         return (
             <div className="window-control__buttons">
                 <div className="window-control__btn" onClick={() => {this.win.minimize()}}>
                     <FontAwesomeIcon icon="window-minimize" />
                 </div>
-                <div className="window-control__btn" onClick={() => {!this.win.isMaximized() ? this.win.maximize() : this.win.unmaximize()}}>
-                    <FontAwesomeIcon icon="window-maximize" />
+                <div className="window-control__btn" onClick={() => {this.windowMaximize()}}>
+                    {!this.state.isMaximized ? <FontAwesomeIcon icon="window-maximize" /> : <FontAwesomeIcon icon="window-restore" />}
                 </div>
                 <div className="window-control__btn" onClick={() => {this.win.close()}}>
                     <FontAwesomeIcon icon="times" />
