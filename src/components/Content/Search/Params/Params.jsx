f71513a931964c117da0c731a67a9b588888695b
uzervlad <vladik0701@gmail.com>
2020-05-11 13:42:35 +0300

    Fix default checked & styles

--- src/components/Content/Search/Params/Params.jsx
+++ src/components/Content/Search/Params/Params.jsx
@@ -7,6 +7,10 @@ import { ReactComponent as Fruits } from '../../../../icons/catch-icon.svg';
 import { ReactComponent as Mania } from '../../../../icons/mania-icon.svg';
 
 export default class Params extends Component {
+    state = {
+        recommended: 7.06
+    };
+
     setActive (e) {
         e.target.className !== 'content-params-row__option--active' ? e.target.className = 'content-params-row__option--active' : e.target.className = 'content-params-row__option'
     }
@@ -14,9 +18,13 @@ export default class Params extends Component {
     render() {
         return (
             <div className="content-params">
-                <div className="content-params-row content-params-row--icon">
-                    <span className="content-params-row__title"></span>
+                <div className="content-params-row">
+                    <span className="content-params-row__title">Mode</span>
                     <div className="content-params-row__options">
+                        <label className="content-params-row__option">
+                            <input type="radio" name="params-modes" defaultChecked />
+                            <span>Any</span>
+                        </label>
                         <label className="content-params-row__option">
                             <input type="radio" name="params-modes"/>
                             <Osu title="" style={{width: '28px', height: '28px'}} />
@@ -40,7 +48,7 @@ export default class Params extends Component {
                     <div className="content-params-row__options">
                         <label className="content-params-row__option">
                             <input type="checkbox" name="params-general" />
-                            <span>{`Recommended difficulty ${7.05}`}</span>
+                            <span>Recommended difficulty ({this.state.recommended})</span>
                         </label>
                         <label className="content-params-row__option">
                             <input type="checkbox" name="params-general" />
@@ -56,7 +64,7 @@ export default class Params extends Component {
                             <span>Any</span>
                         </label>
                         <label className="content-params-row__option">
-                            <input type="radio" name="params-categories" />
+                            <input type="radio" name="params-categories" defaultChecked />
                             <span>Has Leaderboard</span>
                         </label>
                         <label className="content-params-row__option">
